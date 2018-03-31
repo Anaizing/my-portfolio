ed585a523ba4e90d32c8ac072986744de6a69053
Anai Araya <anai.yusary@gmail.com>
2018-03-31 11:45:05 +1100

    add about anai section

--- src/pages/generic.js
+++ src/pages/generic.js
@@ -7,7 +7,7 @@ import pic11 from '../assets/images/pic11.jpg'
 const Generic = (props) => (
     <div>
         <Helmet>
-            <title>Generic - Anai by ANAIZING</title>
+            <title>About Anai </title>
             <meta name="description" content="Generic Page" />
         </Helmet>
 
@@ -15,12 +15,14 @@ const Generic = (props) => (
             <section id="one">
                 <div className="inner">
                     <header className="major">
-                        <h1>Generic</h1>
+                        <h1>Anai Araya</h1>
                     </header>
                     <span className="image main"><img src={pic11} alt="" /></span>
-                    <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+                    <p>An inquisitive Web Developer with a knack for creative problem solving, who strongly believes the internet should be readily accessible to everyone. Therefore understands the importance of accessibility and strives for compliance with WCAG 2.0.</p>
+                    <p>Skilled in various front and back end languages with a strong focus on Javascript and its ecosystem, including Node.js, React, Express.js, Gatsby.js among others and has a firm grasp on package management, version control & bundling tools like Webpack, Grunt, Gulp.</p>
+                    <p>Really enjoys working with components, modular architecture and microservices. When building reusable component libraries, enjoys the use of CSS in JS and CSS Modules for styling, although always open to discuss alternatives.</p>
+                    <p>Has built web apps, reusable component libraries and js starter kits incorporating various testing frameworks & assertion/helper libraries to perform automated unit testing, interaction testing & snapshot testing using tools such as Mocha/Jest and Continuous Intergration tools like TravisCI/appVeyor. Understands how crucial, responsive design & cross device/browser support is today.</p>
+                    <p>Self taught, driven and passionate about keeping up with the latest technologies and tech news. Hungry to grow further, continue learning and determined to succeed. An avid communicator whos friendly and charismatic nature shines through in her professional relationships and collaborations.</p>
                 </div>
             </section>
         </div>
